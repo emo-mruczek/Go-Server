99c1eac1b0aa30b9f4dedd1c5a267fc46862e893
emo-mruczek <110098604+emo-mruczek@users.noreply.github.com>
2024-01-22 21:06:55 +0100

    feat: waiting for each other (so dirty)

--- src/main/java/org/example/ServerApp.java
+++ src/main/java/org/example/ServerApp.java
@@ -12,9 +12,14 @@ public class ServerApp {
       MyLogger.logger.log(Level.INFO, "Server is listening on port 4444");
       while (true) {
         Socket firstPlayer = serverSocket.accept();
+        MessageController.sendMessage("FIRST", firstPlayer);
+        String size = MessageController.receiveMessage(firstPlayer);
         MyLogger.logger.log(Level.INFO, "First client connected");
         Socket secondPlayer = serverSocket.accept();
+        MessageController.sendMessage("SECOND", secondPlayer);
+        MessageController.sendMessage(size, secondPlayer);
         MyLogger.logger.log(Level.INFO, "Second client connected");
+
         Connection task1 = new Connection(firstPlayer);
         Connection task2 = new Connection(secondPlayer);
         Thread t1 = new Thread(task1);
