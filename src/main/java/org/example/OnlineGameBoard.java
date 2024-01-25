ff31730b45beaef9f3cb2af3eeabf7fe6cd5ba5a
emo-mruczek <110098604+emo-mruczek@users.noreply.github.com>
2024-01-25 13:51:42 +0100

    fix: communication

--- src/main/java/org/example/OnlineGameBoard.java
+++ src/main/java/org/example/OnlineGameBoard.java
@@ -1,11 +1,15 @@
 package org.example;
 
 import java.net.Socket;
+import java.util.logging.Level;
 
 public class OnlineGameBoard implements Runnable {
 
   private Socket firstPlayer;
   private Socket secondPlayer;
+
+  private Socket currPlayer;
+  private Socket currOpponent;
 
   private int gameBoard[][];
   private int gameID;
@@ -27,13 +31,72 @@ public class OnlineGameBoard implements Runnable {
     MessageController.sendMessage("JOINED", firstPlayer);
 
     while (true) {
+      currPlayer = firstPlayer;
+      currOpponent = secondPlayer;
       String fromFirst = MessageController.receiveMessage(firstPlayer);
-      MessageController.sendMessage(fromFirst, secondPlayer);
+      insertStone(fromFirst);
 
+      currPlayer = secondPlayer;
+      currOpponent = firstPlayer;
       String fromSecond = MessageController.receiveMessage(secondPlayer);
-      MessageController.sendMessage(fromSecond, firstPlayer);
+      insertStone(fromSecond);
     }
 
+  }
+
+  private void insertStone(String value) {
+    int row = getRow(value.charAt(0));
+    int col = getCol(value.charAt(1));
+    int color = getColor(value.charAt(2));
+
+    makeMove(row, col, color);
+  }
+
+  private void makeMove(int row, int col, int color) {
+    //  if (gameBoard[row][col] != 0) {
+    //   MessageController.sendMessage("INSERT FALSE", socket);
+    //    MyLogger.logger.log(Level.INFO, "Field is already occupied: " + row + col);
+    //   return;
+    //  }
+
+    //  int[][] tempBoard = copyBoard(gameBoard);
+
+    //  tempBoard[row][col] = color;
+
+    //   if (isCapturingMove(row, col, color, tempBoard)) {
+
+    //    gameBoard[row][col] = color;
+    //   MessageController.sendMessage("INSERT TRUE", socket);
+    //   MyLogger.logger.log(Level.INFO, "Stone captured opponent's stone");
+    //   DatabaseConnection.saveMove(prepareStatement(color, row, col, "INSERTION"), gameID);
+
+    //   if (color == 1) {
+    //     blackCaptures++;
+    //    whiteStones--;
+    //   } else {
+    //     whiteCaptures++;
+    //     blackStones--;
+    //  }
+    // } else {
+    //    if (isSuicidalMove(row, col, color, tempBoard)) {
+    //      MessageController.sendMessage("INSERT FALSE", socket);
+    //     MyLogger.logger.log(Level.INFO, "Suicidal move: " + row + col);
+    //   } else {
+    gameBoard[row][col] = color;
+    MessageController.sendMessage("INSERT TRUE", currPlayer);
+    MessageController.sendMessage(row + String.valueOf(col) + color, currPlayer);
+    MessageController.sendMessage("INSERT TRUE", currOpponent);
+    MessageController.sendMessage(row + String.valueOf(col) + color, currOpponent);
+    MyLogger.logger.log(Level.INFO, "Insertion ok: " + row + col);
+    //    DatabaseConnection.saveMove(prepareStatement(color, row, col, "INSERTION"), gameID);
+
+    //     if (color == 1) {
+    //      blackStones++;
+    //    } else if (color == 2) {
+    //      whiteStones++;
+    //    }
+    //  }
+    //  }
   }
 
 
@@ -44,5 +107,34 @@ public class OnlineGameBoard implements Runnable {
       }
     }
   }
+
+  private int getRow(char rowChar) {
+    if (rowChar >= '0' && rowChar <= '9') {
+      return rowChar - '0';
+    } else {
+      return rowChar - 'A' + 10;
+    }
+  }
+
+  private int getCol(char colChar) {
+    if (colChar >= '0' && colChar <= '9') {
+      return colChar - '0';
+    } else {
+      return colChar - 'A' + 10;
+    }
+  }
+
+  private int getColor(char colorChar) {
+    return Character.getNumericValue(colorChar);
+  }
+
+  private String convertPosition(int pos) {
+    if (pos < 10) {
+      return String.valueOf(pos);
+    } else {
+      return String.valueOf((char) ('A' + pos - 10));
+    }
+  }
+
 
 }
